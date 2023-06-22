19555b2edb55f54bfbbd6ddbee1323ed6a183b9c
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-06-21 22:44:26 -0500

    import the data file,initiated some states

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
-
-
+import { useState } from "react"
+import data from './data'
 const App = () => {
+ const [text, setText] = useState([]);
+ const [count, setCount] = useState(0);
+ const [errorMessage, setErrorMessage] = useState("");
+  
   return (
     <div>App</div>
   )
